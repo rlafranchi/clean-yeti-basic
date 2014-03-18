32235f9117d723f077059c5fcdd75a31b9c24eba
Administrator <rlafranchi@serenethemes.com>
2014-03-18 17:54:31 -0600

    v2.2.0

--- library/extensions/theme-options.php
+++ library/extensions/theme-options.php
@@ -34,12 +34,16 @@ function cleanyetibasic_settings_page() {
 
 <h3><?php _e( 'Happy with this Theme', 'cleanyetibasic' ); ?>?</h3>
 <p><?php printf( __('Donating will ensure future support and updates to this theme.  Serene Themes also provides themes that include more features along with extended theme support at a reasonable cost.  Need help getting started? Serene Themes also offers Web Development services to help you get started with your website or provide improvements to your current site.  Find out more at %s.', 'cleanyetibasic' ), '<a href="http://www.serenethemes.com" title="Serene Themes">www.serenethemes.com</a>' ); ?></p>
+<h3><?php _e( 'Donate using PayPal', 'cleanyetibasic' ); ?>:</h3>
 <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
     <input type="hidden" name="cmd" value="_s-xclick">
     <input type="hidden" name="hosted_button_id" value="YEC2X426PKBGA">
     <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
     <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
 </form>
+
+<h3><?php _e( 'Serene Themes also accepts Bitcoin donations', 'cleanyetibasic' ); ?></h3>
+<a href="bitcoin:12tHYy7GqmrvmHLUk549VSuqKr6no1RXTY">BTC: 12tHYy7GqmrvmHLUk549VSuqKr6no1RXTY</a>
 
 <h2><?php _e( 'Clean Yeti Basic Theme Options', 'cleanyetibasic' ); ?></h2>
 <form method="post" action="options.php">
