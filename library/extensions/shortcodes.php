d131cb39fb9b831b904b5d16f4ad52491defd831
Administrator <rlafranchi@serenethemes.com>
2014-03-20 21:56:43 -0600

    v2.2.1

--- library/extensions/shortcodes.php
+++ library/extensions/shortcodes.php
@@ -118,8 +118,6 @@ function cleanyetibasic_gallery_shortcode_custom($attr) {
 		}
 		$output .= "</{$itemtag}>";
 
-        if ( $columns > 0 && ++$i % $columns == 0 )
-			$output .= '<br style="clear: both" />';
 	}
 
 	$output .= "</ul>\n";
