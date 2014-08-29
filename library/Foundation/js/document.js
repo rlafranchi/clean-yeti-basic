6b621b42671744ec7b29ff67118174b5906598d8
rlafranchi <rlafranchi@serenethemes.com>
2014-08-29 16:45:09 -0600

    sticky footer

--- library/Foundation/js/document.js
+++ library/Foundation/js/document.js
@@ -5,4 +5,29 @@ jQuery(document).ready( function ($) {
             var ddid = $(this).attr('id');
             $(this).find('.content').attr('id', 'panel-' + ddid);
     });
+        var footerHeight = $('#footer').height();
+        var mainHeight = $('#main').outerHeight();
+        var headerHeight = $('#header').outerHeight();
+        var winHeight = $(window).height();
+        var totalHeight = footerHeight + mainHeight + headerHeight;
+        if (totalHeight < winHeight ) {
+          var marHeight = winHeight - totalHeight;
+          $('#footer').css({ 'margin-top' : (marHeight - 20) + 'px' });
+        }	else {
+          $('#footer').removeAttr('style');
+        }
+    $(window).resize(function(){
+        footerHeight = $('#footer').height();
+        mainHeight = $('#main').outerHeight();
+        headerHeight = $('#header').outerHeight();
+        winHeight = $(window).height();
+        totalHeight = footerHeight + mainHeight + headerHeight;
+        if (totalHeight < winHeight ) {
+          var marHeight = winHeight - totalHeight;
+          $('#footer').css({ 'margin-top' : (marHeight - 20) + 'px' });
+        }	else {
+          $('#footer').removeAttr('style');
+        }
+        $(window).resize();
+    });
 });
